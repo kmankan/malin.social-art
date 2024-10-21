44c793b35642f086fb20646e1f66d01de45bf85d
mahlen <malink027@gmail.com>
2024-10-21 18:05:55 -0400

    user can modify various config elements now

--- frontend/src/app/components/RotatingBoxes.tsx
+++ frontend/src/app/components/RotatingBoxes.tsx
@@ -1,9 +1,7 @@
 'use client';
-
 import { useRef, useState, useEffect } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { OrbitControls, useDrag } from '@react-three/drei'
-import * as THREE from 'three'
+import { Canvas, useFrame, ThreeEvent } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 interface BoxConfig {
   id: string;
@@ -12,42 +10,33 @@ interface BoxConfig {
   size: number;
   rotationAxis: 'x' | 'y';
   position: [number, number, number];
+  isSelected: boolean;
 }
 
-function Box({ id, position, color, speed, size, rotationAxis, onPositionChange }) {
+function Box({ position, color, speed, size, rotationAxis, isSelected, onSelect }) {
+  // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
+  // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  const { camera } = useThree()
-
+  // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
     if (ref.current) {
       ref.current.rotation[rotationAxis] += speed * delta
     }
   })
-
-  const bind = useDrag(({ offset: [x, y] }) => {
-    const unprojectedPoint = new THREE.Vector3(x, y, 0)
-    unprojectedPoint.unproject(camera)
-    const dir = unprojectedPoint.sub(camera.position).normalize()
-    const distance = -camera.position.z / dir.z
-    const pos = camera.position.clone().add(dir.multiplyScalar(distance))
-    onPositionChange(id, [pos.x, pos.y, pos.z])
-  }, { pointerEvents: true })
-
+  // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh
       position={position}
       ref={ref}
       scale={clicked ? size * 1.5 : size}
-      onClick={(event) => {
-        event.stopPropagation()
-        click(!clicked)
+      onClick={(event: ThreeEvent<MouseEvent>) => {
+        click(!clicked);
+        onSelect(event);
       }}
       onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}
-      {...bind()}
-    >
+      onPointerOut={(event) => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : color} />
     </mesh>
@@ -56,8 +45,8 @@ function Box({ id, position, color, speed, size, rotationAxis, onPositionChange 
 
 export default function RotatingBoxes() {
   const [boxes, setBoxes] = useState<BoxConfig[]>([
-    { id: 'box1', color: 'lime', speed: 1, size: 1, rotationAxis: 'x', position: [-2, 0, 0] },
-    { id: 'box2', color: 'cyan', speed: 0.5, size: 1.2, rotationAxis: 'y', position: [1.2, 0, 0] },
+    { id: 'box1', color: '#00ff00', speed: 1, size: 1, rotationAxis: 'x', position: [-2, 0, 0], isSelected: false },
+    { id: 'box2', color: '#00ffff', speed: 0.5, size: 1.2, rotationAxis: 'y', position: [1.2, 0, 0], isSelected: false },
   ]);
   const [selectedBox, setSelectedBox] = useState('box1');
 
@@ -85,6 +74,13 @@ export default function RotatingBoxes() {
     ));
   };
 
+  const handleSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSize = parseFloat(event.target.value);
+    setBoxes(prevBoxes => prevBoxes.map(box =>
+      box.id === selectedBox ? { ...box, size: newSize } : box
+    ));
+  };
+
   const handleAddBox = () => {
     const newBoxId = `box${boxes.length + 1}`;
     const newBox: BoxConfig = {
@@ -94,87 +90,92 @@ export default function RotatingBoxes() {
       size: Math.random() * 0.5 + 0.8,
       rotationAxis: Math.random() > 0.5 ? 'x' : 'y',
       position: [Math.random() * 4 - 2, Math.random() * 4 - 2, Math.random() * 4 - 2],
+      isSelected: false,
     };
     setBoxes(prevBoxes => [...prevBoxes, newBox]);
     setSelectedBox(newBoxId);
   };
 
-  const handlePositionChange = (id: string, newPosition: [number, number, number]) => {
-    setBoxes(prevBoxes => prevBoxes.map(box =>
-      box.id === id ? { ...box, position: newPosition } : box
-    ))
-  }
+  const handleBoxClick = (boxId: string) => {
+    setSelectedBox(boxId);
+    setBoxes(prevBoxes => prevBoxes.map(box => ({
+      ...box,
+      isSelected: box.id === boxId
+    })));
+  };
 
   return (
-    <div>
-      <div className="flex flex-col">
-        <div className='rotating-box flex-grow border-2'>
-          <Canvas>
-            <ambientLight intensity={Math.PI / 3} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            {boxes.map(box => (
-              <Box key={box.id} {...box} onPositionChange={handlePositionChange} />
-            ))}
-            <OrbitControls makeDefault />
-          </Canvas>
-        </div>
-        <div className='userConfig flex-col items-center border-2'>
-          <div className='text-sm font-mono'>Modify</div>
-          <div className='addBox mb-4'>
-            <label htmlFor="addBox" className='text-sm font-mono block mb-2'>Add Box:</label>
-            <button id="addBox" className='w-full p-2 border rounded' onClick={handleAddBox}>Add Box</button>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor="boxSelect" className='text-sm font-mono block mb-2'>Select Box:</label>
-            <select id="boxSelect" className='w-full p-2 border rounded' onChange={handleBoxSelect} value={selectedBox}>
-              {boxes.map(box => (
-                <option key={box.id} value={box.id}>{box.id}</option>
-              ))}
-            </select>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor="colorSelect" className='text-sm font-mono block mb-2'>Color:</label>
-            <div className="relative w-full h-10 rounded overflow-hidden">
-              <input
-                type="color"
-                id="colorPicker"
-                className="absolute w-full h-full opacity-0 cursor-pointer"
-                onChange={handleColorSelect}
-                value={selectedBox === 'box1' ? boxes[0].color : boxes[1].color}
-              />
-              <div
-                className="w-full h-full"
-                style={{
-                  background: `linear-gradient(to right, 
-                    #FF0000, /* Red */
-                    #FF7F00, /* Orange */
-                    #FFFF00, /* Yellow */
-                    #00FF00, /* Green */
-                    #0000FF, /* Blue */
-                    #8B00FF  /* Violet */
-                  )`,
-                }}
-              ></div>
-            </div>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor="speedInput" className='text-sm font-mono block mb-2'>Speed:</label>
-            <input
-              type="range"
-              id="speedInput"
-              min="0"
-              max="10"
-              step="0.2"
-              className='w-full'
-              onChange={handleSpeedChange}
-              value={boxes.find(box => box.id === selectedBox)?.speed || 0}
+    <div className="flex flex-col h-screen">
+      <div className='rotating-box flex-grow border-2'>
+        <Canvas>
+          <ambientLight intensity={Math.PI / 3} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          {boxes.map(box => (
+            <Box
+              key={box.id}
+              {...box}
+              isSelected={box.id === selectedBox}
+              onSelect={() => handleBoxClick(box.id)}
             />
+          ))}
+          <OrbitControls />
+        </Canvas>
+      </div>
+      <div className='userConfig flex-col items-center border-2 p-4'>
+        <div className='addBox mb-4'>
+          <label htmlFor="addBox" className='text-sm font-mono block mb-2'>Add Box:</label>
+          <button id="addBox" className='w-full p-2 border rounded' onClick={handleAddBox}>Add Box</button>
+        </div>
+        <div className='mb-4'>
+          <label htmlFor="boxSelect" className='text-sm font-mono block mb-2'>Select Box:</label>
+          <select id="boxSelect" className='w-full p-2 border rounded' onChange={handleBoxSelect} value={selectedBox}>
+            {boxes.map(box => (
+              <option key={box.id} value={box.id}>{box.id}</option>
+            ))}
+          </select>
+        </div>
+        <div className='mb-4'>
+          <label htmlFor="colorSelect" className='text-sm font-mono block mb-2'>Color:</label>
+          <div className="relative w-full h-10 rounded overflow-hidden">
+            <input
+              type="color"
+              id="colorPicker"
+              className="absolute w-full h-full opacity-0 cursor-pointer"
+              onChange={handleColorSelect}
+              value={selectedBox === 'box1' ? boxes[0].color : boxes[1].color}
+            />
+            <div
+              className="w-full h-full"
+              style={{
+                background: `linear-gradient(to right, 
+                  #FF0000, /* Red */
+                  #FF7F00, /* Orange */
+                  #FFFF00, /* Yellow */
+                  #00FF00, /* Green */
+                  #0000FF, /* Blue */
+                  #8B00FF  /* Violet */
+                )`,
+              }}
+            ></div>
           </div>
-          <div>
-            <label htmlFor="flipAxis" className='text-sm font-mono block mb-2'>Axis:</label>
-            <button id="flipAxis" className='w-full p-2 border rounded' onClick={handleFlipAxis}>Flip</button>
-          </div>
+        </div>
+        <div className='mb-4'>
+          <label htmlFor="speedInput" className='text-sm font-mono block mb-2'>Speed:</label>
+          <input
+            type="range"
+            id="speedInput"
+            min="0"
+            max="10"
+            step="0.2"
+            className='w-full'
+            onChange={handleSpeedChange}
+            value={boxes.find(box => box.id === selectedBox)?.speed || 0}
+          />
+        </div>
+        <div>
+          <label htmlFor="flipAxis" className='text-sm font-mono block mb-2'>Axis:</label>
+          <button id="flipAxis" className='w-full p-2 border rounded' onClick={handleFlipAxis}>Flip</button>
         </div>
       </div>
     </div>
