0256debf0ca2262a4e560bbd22022285eda7e3ad
mahlen <malink027@gmail.com>
2024-10-24 22:36:12 -0400

    create component for rotating boxes canvas and sort out typing for config state management in component

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,17 +1,23 @@
-
+import { prisma } from '@/lib/db/index'
+import { Artwork, User } from '@prisma/client'
+import { RotatingBoxesCanvas } from './components/ui/RotatingBoxesCanvas';
+import { AnimationState } from '@/types';
 
 export default async function Page() {
-
-
+  const artworks = (await prisma.artwork.findMany({ // returns an array of artwork objects
+    include: { author: true }  // This will include the author details for each artwork
+  })) as (Artwork & { author: User, state: AnimationState })[];
 
   return (
     <div className="flex flex-col min-h-screen bg-rose-50">
       <main className="flex-grow p-8border-2">
-        {/* Your custom component will go here */}
-        <div className="feed p-6 rounded-lg shadow-lg flex justify-center items-center min-h-screen w-2/3 mx-auto">
-          <div className="bg-white p-6 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300">
-            <h2 className="text-2xl font-semibold text-gray-800 mb-4 font-mono">Your Social Art Works</h2>
-            <p className="text-gray-600">Start creating and sharing your artistic journey today!</p>
+        <div className="feed p-6 rounded-lg shadow-lg flex justify-center items-start min-h-screen w-2/3 mx-auto">
+          <div className="artwork flex justify-center items-center bg-white p-12 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300 w-full max-w-6xl min-h-[400px]">
+            {artworks.map((canvas, index) => {
+              return (
+                <RotatingBoxesCanvas initialBoxes={canvas.state.boxes} initialBackgroundColor={canvas.state.backgroundColor} initialSelectedBox={canvas.state.selectedBoxId} />
+              )
+            })}
           </div>
         </div>
       </main>
